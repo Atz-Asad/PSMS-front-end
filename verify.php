d18fdcff8c14d0d4b68f720c0433146c5071d2ad
Asaduzzaman <ashad731386@gmail.com>
2022-06-21 23:14:01 +0600

    psms front end email verify update

--- verify.php
+++ verify.php
@@ -195,9 +195,9 @@ if(isset($_POST['st_mobile_verify_btn'])){
                         $email_status = Student('is_email_verified',$_SESSION['st_loggedin'][0]['id']);
                         $mobile_status = Student('is_mobile_verified',$_SESSION['st_loggedin'][0]['id']);
 
-                        if(isset($_SESSION['st_loggedin']) AND $email_status == 1 AND $mobile_status == 1){
-                            header('location:deshbord/index.php');
-                        }
+                        // if(isset($_SESSION['st_loggedin']) AND $email_status == 1 AND $mobile_status == 1){
+                        //     header('location:deshbord/index.php');
+                        // }
                     ?>
                     <p>Email:<?php
                     if($email_status == 1){
